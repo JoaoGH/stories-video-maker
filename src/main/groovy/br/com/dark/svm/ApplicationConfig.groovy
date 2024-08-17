bd66f6e1d68f8e876aacfad3cf4740d9eb55ea64
JoaoGH <gabrielhartmannqwert@gmail.com>
2024-08-16 23:41:23 -0300

    feat: obter config do caminho dos videos.
    Obter caminho base dos videos de input

--- src/main/groovy/br/com/dark/svm/ApplicationConfig.groovy
+++ src/main/groovy/br/com/dark/svm/ApplicationConfig.groovy
@@ -1,9 +1,20 @@
 package br.com.dark.svm
 
+import grails.config.Config
+import grails.util.Holders
+
 class ApplicationConfig {
+
+    static Config getConfig() {
+        return Holders.getConfig()
+    }
 
     static String getRedditAITMUrl() {
         return "https://www.reddit.com/r/EuSouOBabaca.json"
+    }
+
+    static String getVideoBasePath() {
+        return getConfig().getProperty("video.base.path", String)
     }
 
 }
